45b1487398681038a45f06e90f22866b39cab9e9
kartikesh95 <kartikesh.nadar@gmail.com>
2022-05-06 17:25:41 +0530

    c

--- azpolicy.tf
+++ azpolicy.tf
@@ -1,6 +1,6 @@
 resource "azurerm_subscription_policy_assignment" "auditvms1" { 
  name = "audit-vm-manageddisks" 
- subscription_id = "5cc96e7f-0fbb-4109-933a-42f31f8deee8"
+ subscription_id = "/subscriptions/5cc96e7f-0fbb-4109-933a-42f31f8deee8"
  policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/06a78e20-9358-41c9-923c-fb736d382a4d" 
  description = "Shows all virtual machines not using managed disks" 
  display_name = "Audit VMs without managed disks assignment" 
